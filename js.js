aed6adc0332b667bb948b979a0d445f0fad71dfc
Noyabrskaia <rudenko.a.design@gmail.com>
2020-11-20 12:17:52 +0200

    2nd commit

--- js.js
+++ js.js
@@ -2,14 +2,21 @@ function insert(nmbrs) {
     document.querySelector('.output').value = document.querySelector('.output').value+nmbrs;
   }
   
+  function clean() {
+    document.querySelector('.output').value="";
+  }
+
+  function back() {
+    let exp = document.querySelector('.output').value;
+    document.querySelector('.output').value = exp.substring (0, exp.length-1);
+  } 
+
  function equal() {
     let expresion = document.querySelector('.output').value;
   
     if(expresion) {
-      document.querySelector('.output').value = eval(document.querySelector('.output').value);
+      document.querySelector('.output').value = eval(expresion);
     }
   } 
-  
-  function clean() {
-    document.querySelector('.output').value="";
-  }
+
+ 
